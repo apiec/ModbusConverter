10b7fae26026d5f436639e485f0a45877ccadc70
apiec <apieczul97@gmail.com>
2021-04-25 00:14:47 +0200

    Change the port used by gui to 80

--- ModbusConverter_AspNet/Program.cs
+++ ModbusConverter_AspNet/Program.cs
@@ -23,7 +23,7 @@ namespace ModbusConverter
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:5000");
+                    webBuilder.UseUrls("http://*:80");
                 })
                 .ConfigureServices(services =>
                 {
