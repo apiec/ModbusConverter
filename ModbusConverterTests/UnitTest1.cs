3ea32563725140f9afdff64a7db8b6338c669d13
apiec <apieczul97@gmail.com>
2022-03-15 23:21:05 +0100

    Add a ModbusConverterTests project

---
+++ ModbusConverterTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using FluentAssertions;
+
+namespace ModbusConverterTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
